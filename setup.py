793dd3a24e665b1ea45cbf23dd79986c4b404faf
Agustin Barto <abarto@gmail.com>
2016-08-05 15:53:18 -0300

    Add LICENSE. Update setup script.

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
+from os import path
 from setuptools import setup
+
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(name='django_uncertainty',
       version='0.1',
       description='A Django application to generate predictable errors on sites',
+      long_description=long_description,
       author='Agustin Barto',
       author_email='abarto@gmail.com',
       url='https://github.com/abarto/django_uncertainty',
+      license='BSD',
       install_requires=[],
       classifiers=[
           'Development Status :: 4 - Beta',
