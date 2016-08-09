1920bbeb6209bc71a3f387a329558fb00a96a312
Agustin Barto <abarto@gmail.com>
2016-08-09 16:08:54 -0300

    Add unit tests

--- uncertainty/tests/test_conditions.py
+++ uncertainty/tests/test_conditions.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from unittest.mock import MagicMock, patch
 
-from uncertainty.conditions import Predicate, NotPredicate, OrPredicate, AndPredicate
+from uncertainty.conditions import (Predicate, NotPredicate, OrPredicate, AndPredicate,
+                                    IsMethodPredicate, is_get, is_delete, is_post, is_put)
 
 
 class PredicateTests(TestCase):
@@ -170,11 +171,90 @@ class AndPredicateTests(TestCase):
         self.assertTrue(self.and_predicate(self.get_response_mock, self.request_mock))
 
 
-# TODO Add IsMethodPredicate tests
-# TODO Add is_get tests
-# TODO Add is_delete tests
-# TODO Add is_post tests
-# TODO Add is_put tests
+class IsMethodPredicateTests(TestCase):
+    def setUp(self):
+        self.some_method = MagicMock()
+        self.is_method_predicate = IsMethodPredicate(self.some_method)
+        self.get_response_mock = MagicMock()
+        self.request_mock = MagicMock()
+
+    def test_returns_true_if_request_method_is_encapsulated_method(self):
+        """Tests that calling IsMethodPredicate returns True if the request method is the same as
+        the encapsulated method"""
+        self.request_mock.method = self.some_method
+        self.assertTrue(self.is_method_predicate(self.get_response_mock, self.request_mock))
+
+    def test_returns_false_if_request_method_is_nt_encapsulated_method(self):
+        """Tests that calling IsMethodPredicate returns False if the request method is not the same
+        as the encapsulated method"""
+        self.request_mock.method = MagicMock()
+        self.assertFalse(self.is_method_predicate(self.get_response_mock, self.request_mock))
+
+
+class IsGetTests(TestCase):
+    def setUp(self):
+        self.get_response_mock = MagicMock()
+        self.request_mock = MagicMock()
+
+    def test_returns_true_if_request_method_is_get(self):
+        """Tests that calling is_get returns True if the request method is 'GET'"""
+        self.request_mock.method = 'GET'
+        self.assertTrue(is_get(self.get_response_mock, self.request_mock))
+
+    def test_returns_false_if_request_method_is_not_get(self):
+        """Tests that calling is_get returns False if the request method is not 'GET'"""
+        self.request_mock.method = 'FOOBAR'
+        self.assertFalse(is_get(self.get_response_mock, self.request_mock))
+
+
+class IsDeleteTests(TestCase):
+    def setUp(self):
+        self.get_response_mock = MagicMock()
+        self.request_mock = MagicMock()
+
+    def test_returns_true_if_request_method_is_delete(self):
+        """Tests that calling is_delete returns True if the request method is 'DELETE'"""
+        self.request_mock.method = 'DELETE'
+        self.assertTrue(is_delete(self.get_response_mock, self.request_mock))
+
+    def test_returns_false_if_request_method_is_not_delete(self):
+        """Tests that calling is_delete returns False if the request method is not 'DELETE'"""
+        self.request_mock.method = 'FOOBAR'
+        self.assertFalse(is_delete(self.get_response_mock, self.request_mock))
+
+
+class IsPostTests(TestCase):
+    def setUp(self):
+        self.get_response_mock = MagicMock()
+        self.request_mock = MagicMock()
+
+    def test_returns_true_if_request_method_is_post(self):
+        """Tests that calling is_post returns True if the request method is 'POST'"""
+        self.request_mock.method = 'POST'
+        self.assertTrue(is_post(self.get_response_mock, self.request_mock))
+
+    def test_returns_false_if_request_method_is_not_post(self):
+        """Tests that calling is_post returns False if the request method is not 'POST'"""
+        self.request_mock.method = 'FOOBAR'
+        self.assertFalse(is_post(self.get_response_mock, self.request_mock))
+
+
+class IsPutTests(TestCase):
+    def setUp(self):
+        self.get_response_mock = MagicMock()
+        self.request_mock = MagicMock()
+
+    def test_returns_true_if_request_method_is_post(self):
+        """Tests that calling is_put returns True if the request method is 'PUT'"""
+        self.request_mock.method = 'PUT'
+        self.assertTrue(is_put(self.get_response_mock, self.request_mock))
+
+    def test_returns_false_if_request_method_is_not_post(self):
+        """Tests that calling is_put returns False if the request method is not 'PUT'"""
+        self.request_mock.method = 'FOOBAR'
+        self.assertFalse(is_put(self.get_response_mock, self.request_mock))
+
+
 # TODO Add HasRequestParameterPredicate tests
 # TODO Add PathMatchesRegexpPredicate tests
 # TODO Add IsAuthenticatedPredicate tests
