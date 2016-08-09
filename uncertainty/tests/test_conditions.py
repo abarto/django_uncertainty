000dffc609f6f37540d43a871d38553365ca3267
Agustin Barto <abarto@gmail.com>
2016-08-09 15:48:09 -0300

    Add unit tests

--- uncertainty/tests/test_conditions.py
+++ uncertainty/tests/test_conditions.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from unittest.mock import MagicMock, patch
 
-from uncertainty.conditions import Predicate
+from uncertainty.conditions import Predicate, NotPredicate, OrPredicate, AndPredicate
 
 
 class PredicateTests(TestCase):
@@ -48,9 +48,128 @@ class PredicateTests(TestCase):
         with patch('uncertainty.conditions.AndPredicate') as and_predicate_mock:
             self.assertEqual(and_predicate_mock.return_value, self.predicate & self.other_predicate)
 
-# TODO Add NotPredicate tests
-# TODO Add OrPredicate tests
-# TODO Add AndPredicate tests
+
+class NotPredicateTests(TestCase):
+    def setUp(self):
+        self.some_predicate = MagicMock()
+        self.not_predicate = NotPredicate(self.some_predicate)
+        self.get_response_mock = MagicMock()
+        self.request_mock = MagicMock()
+
+    def test_calls_encapsulated_predicate(self):
+        """Tests that calling the NotPredicate calls the encapsulated predicate"""
+        self.not_predicate(self.get_response_mock, self.request_mock)
+        self.some_predicate.assert_called_once_with(self.get_response_mock, self.request_mock)
+
+    def test_returns_false_when_encapsulated_predicate_returns_true(self):
+        """Tests that calling the NotPredicate returns False when the encapsulated predicate
+        returns True"""
+        self.some_predicate.return_value = True
+        self.assertFalse(self.not_predicate(self.get_response_mock, self.request_mock))
+
+    def test_returns_true_when_encapsulated_predicate_returns_false(self):
+        """Tests that calling the NotPredicate returns True when the encapsulated predicate
+        returns False"""
+        self.some_predicate.return_value = False
+        self.assertTrue(self.not_predicate(self.get_response_mock, self.request_mock))
+
+
+class OrPredicateTests(TestCase):
+    def setUp(self):
+        self.some_left_predicate = MagicMock()
+        self.some_right_predicate = MagicMock()
+        self.or_predicate = OrPredicate(self.some_left_predicate, self.some_right_predicate)
+        self.get_response_mock = MagicMock()
+        self.request_mock = MagicMock()
+
+    def test_calls_left_encapsulated_predicate(self):
+        """Tests that calling the OrPredicate calls the left encapsulated predicate"""
+        self.or_predicate(self.get_response_mock, self.request_mock)
+        self.some_left_predicate.assert_called_once_with(self.get_response_mock, self.request_mock)
+
+    def test_doesnt_call_right_encapsulated_predicate_if_left_is_true(self):
+        """Tests that calling the OrPredicate doesn't call the right encapsulated predicate if
+        calling the left predicate returns True"""
+        self.some_left_predicate.return_value = True
+        self.or_predicate(self.get_response_mock, self.request_mock)
+        self.some_right_predicate.assert_not_called()
+
+    def test_calls_right_encapsulated_predicate_if_left_is_false(self):
+        """Tests that calling the OrPredicate calls the right encapsulated predicate if
+        calling the left predicate returns False"""
+        self.some_left_predicate.return_value = False
+        self.or_predicate(self.get_response_mock, self.request_mock)
+        self.some_left_predicate.assert_called_once_with(self.get_response_mock, self.request_mock)
+
+    def test_returns_true_if_left_encapsulated_predicate_is_true(self):
+        """Tests that calling the OrPredicate returns True if calling the left encapsulated
+        predicate is True"""
+        self.some_left_predicate.return_value = True
+        self.assertTrue(self.or_predicate(self.get_response_mock, self.request_mock))
+
+    def test_returns_true_if_right_encapsulated_predicate_is_true(self):
+        """Tests that calling the OrPredicate returns True if calling the right encapsulated
+        predicate is True"""
+        self.some_right_predicate.return_value = True
+        self.assertTrue(self.or_predicate(self.get_response_mock, self.request_mock))
+
+    def test_returns_false_if_encapsulated_predicates_are_false(self):
+        """Tests that calling the OrPredicate returns False if calling both encapsulated predicates
+        return False"""
+        self.some_left_predicate.return_value = False
+        self.some_right_predicate.return_value = False
+        self.assertFalse(self.or_predicate(self.get_response_mock, self.request_mock))
+
+
+class AndPredicateTests(TestCase):
+    def setUp(self):
+        self.some_left_predicate = MagicMock()
+        self.some_right_predicate = MagicMock()
+        self.and_predicate = AndPredicate(self.some_left_predicate, self.some_right_predicate)
+        self.get_response_mock = MagicMock()
+        self.request_mock = MagicMock()
+
+    def test_calls_left_encapsulated_predicate(self):
+        """Tests that calling the AndPredicate calls the left encapsulated predicate"""
+        self.and_predicate(self.get_response_mock, self.request_mock)
+        self.some_left_predicate.assert_called_once_with(self.get_response_mock,
+                                                         self.request_mock)
+
+    def test_doesnt_call_right_encapsulated_predicate_if_left_is_false(self):
+        """Tests that calling the AndPredicate doesn't call the right encapsulated predicate if
+        calling the left predicate returns False"""
+        self.some_left_predicate.return_value = False
+        self.and_predicate(self.get_response_mock, self.request_mock)
+        self.some_right_predicate.assert_not_called()
+
+    def test_calls_right_encapsulated_predicate_if_left_is_true(self):
+        """Tests that calling the AndPredicate calls the right encapsulated predicate if
+        calling the left predicate returns False"""
+        self.some_left_predicate.return_value = True
+        self.and_predicate(self.get_response_mock, self.request_mock)
+        self.some_left_predicate.assert_called_once_with(self.get_response_mock,
+                                                         self.request_mock)
+
+    def test_returns_false_if_left_encapsulated_predicate_is_false(self):
+        """Tests that calling the AndPredicate returns False if calling the left encapsulated
+        predicate is False"""
+        self.some_left_predicate.return_value = False
+        self.assertFalse(self.and_predicate(self.get_response_mock, self.request_mock))
+
+    def test_returns_false_if_right_encapsulated_predicate_is_false(self):
+        """Tests that calling the AndPredicate returns True if calling the right encapsulated
+        predicate is True"""
+        self.some_right_predicate.return_value = False
+        self.assertFalse(self.and_predicate(self.get_response_mock, self.request_mock))
+
+    def test_returns_true_if_encapsulated_predicates_are_true(self):
+        """Tests that calling the AndPredicate returns True if calling both encapsulated predicates
+        return True"""
+        self.some_left_predicate.return_value = True
+        self.some_right_predicate.return_value = True
+        self.assertTrue(self.and_predicate(self.get_response_mock, self.request_mock))
+
+
 # TODO Add IsMethodPredicate tests
 # TODO Add is_get tests
 # TODO Add is_delete tests
